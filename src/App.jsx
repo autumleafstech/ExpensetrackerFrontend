653f488f0b7a7540e39b864ba5e55926a4998ab6
Rahul Wankhede <wankhederahul116@gmail.com>
2026-03-06 14:02:18 +0530

    create addexpense function delete expense funtion

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,56 @@
+import { useEffect, useState } from 'react'
 import './App.css'
+import ExpenseHeader from './components/ExpenseHeader'
 import ExpenseTable from './components/ExpenseTable'
+import axios from 'axios';
+import ExpenseSummary from './components/ExpenseSummary';
 
 function App() {
-  
+    const [expenses , setExpenses] = useState([]);
+    const [vendorName, setVendorName] = useState("")
+    const fetchExpenses = async () => {
+         try{
+         const response = await axios.get("http://localhost:3000/api/getexpenses");
+        console.log(response.data)
+        setExpenses(response.data)
+         }
+         catch(err) {
+            console.error("Error while fetchinf expense", err)
+         }
+    }
 
+    useEffect(() => {
+      fetchExpenses();
+    },[])
+ function handleVendorName(e) {
+      setVendorName(e.target.value);
+      console.log(e.target.value);
+  }
+  
+    // const filteredData = expenses.filter(
+    //   (exp) => exp.vendor.toLowerCase().includes(vendorName.toLowerCase()));
+    // console.log(filteredData);
+const filteredExpense = vendorName? expenses.filter((exp) => exp.vendor.toLowerCase().includes(vendorName.toLowerCase())): expenses;
+   async function deleteExpense(expenseId) {
+  try {
+    console.log(`http://localhost:3000/api/expenses/${expenseId}`);
+    const response = await axios.delete(`http://localhost:3000/api/deleteexpense/${expenseId}`);
+    setExpenses((prev) => prev.filter((exp) => exp.id !== expenseId));
+    console.log("Deleted:", response.data.id);
+    return response.data;
+  } catch (error) {
+    console.error("Error deleting expense:", error);
+  }
+} 
+
+function handleAddExpense() {
+    fetchExpenses();
+}
   return (
     <>
-    <h1>Expense Tracker </h1>
-    <button>Add Expense</button>
-      <ExpenseTable/>
+      <ExpenseHeader onAdd={handleAddExpense}/>
+      <ExpenseSummary  expenses={expenses} handleVendorName={handleVendorName}/>
+      <ExpenseTable expenses={filteredExpense} onDelete={deleteExpense}/>
     </>
   )
 }
